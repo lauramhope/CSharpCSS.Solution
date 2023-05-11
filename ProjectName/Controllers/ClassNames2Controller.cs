f957df5ccaa926e8e9bde5de4c2de5e1488b3063
Laura Hope <ssteamlaura@gmail.com>
2023-05-11 14:32:01 -0700

    Add template info

---
+++ ProjectName/Controllers/ClassNames2Controller.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjectName.Models;
+using System.Collections.Generic;
+
+namespace ProjectName.Controllers
+{
+  public class ClassNames2Controller : Controller
+  {
+
+    // [HttpGet("/categories/{categoryId}/items/new")]
+    // public ActionResult New(int categoryId)
+    // {
+    //   Category category = Category.Find(categoryId);
+    //   return View(category);
+    // }
+
+    // [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    // public ActionResult Show(int categoryId, int itemId)
+    // {
+    //   Item item = Item.Find(itemId);
+    //   Category category = Category.Find(categoryId);
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   model.Add("item", item);
+    //   model.Add("category", category);
+    //   return View(model);
+    // }
+  }
+}
