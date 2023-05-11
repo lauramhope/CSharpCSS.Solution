20296d6de5a437f4d3916207d8d58b8f19f030fa
Laura Hope <ssteamlaura@gmail.com>
2023-05-11 14:35:44 -0700

    Add more template

---
+++ ProjectName.Tests/ModelTests/ClassName1Tests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ProjectName.Tests
+{
+  // [TestClass]
+  // public class ClassName1Tests : IDisposable
+  // {
+
+  //   public void Dispose()
+  //   {
+  //     ClassName1.ClearAll();
+  //   }
+  // }
+}
---
+++ ProjectName.Tests/ModelTests/ClassName2Tests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectName.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ProjectName.Tests
+{
+  // [TestClass]
+  // public class ClassName2Tests : IDisposable
+  // {
+
+  //   public void Dispose()
+  //   {
+  //     ClassName2.ClearAll();
+  //   }
+  // }
+}
